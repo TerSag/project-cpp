913bb2ad4cd40ae7bdc827cdaf0dae074f37bb8c
Tersag <sashka.tereshuk@gmail.com>
2024-11-08 09:27:38 +0200

    Initial commit

---
+++ task/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+ int main () 
+{
+   int a;
+   int b;
+
+   std::cin >> a >> b;
+   /*
+    for(int j = 1; < a; j = j+1)
+    {
+       for(int i = 1; i < b; i = d+1) {
+               std::cout << "*";
+       } 
+       
+
+
+
+    }
+
+
+ return 0;
+}	
